0f27465d4b153536ebf26a3cb486136bc7befb6a
Christopher Ling Carragher <christopher.ling.carragher@gmail.com>
2013-04-28 13:01:02 +0200

    Moved the target blank to only apply to external links.

--- inject.js
+++ inject.js
@@ -2,8 +2,8 @@ var links = document.querySelectorAll('.title > a');
 Array.prototype.forEach.call( links, function( link, i ) {
     if ( link.innerText !== 'More' ) {
         link.setAttribute( 'tabindex', i + 1 );
+        link.setAttribute( 'target', '_blank' );
     } else {
         link.setAttribute( 'tabindex', links.length + 1 );
     }
-    link.setAttribute( 'target', '_blank' );
 });
